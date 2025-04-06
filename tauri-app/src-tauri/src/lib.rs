35a18dce2c3f4cc47f8c0885724305dee8a86241
oneriang <oneriang@126.com>
2025-04-07 06:59:18 +0900

    Update lib.rs

--- tauri-app/src-tauri/src/lib.rs
+++ tauri-app/src-tauri/src/lib.rs
@@ -1,19 +1,24 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
+fn greet2(search: &str,email: &str,username: &str,password: &str) -> String {
+    format!("你好, {}{}{}{}! You've been greeted from Rust!", search,email,username,password)
+}
+
+#[tauri::command]
 fn greet(name: &str) -> String {
     format!("你好, {}! You've been greeted from Rust!", name)
 }
 
 #[tauri::command]
 fn greet1(id: &str) -> String {
-    format!("
-        <button> {0} </butoon>
-        <button> {0} </butoon>
-        <button> {0} </butoon>
-        <button> {0} </butoon>
-        <button> {0} </butoon>
-        <button> {0} </butoon>
-    ", id)
+    format!(r#"
+        <button class="btn"> {0} </butoon>
+        <button class="btn"> {0} </butoon>
+        <button class="btn"> {0} </butoon>
+        <button class="btn"> {0} </butoon>
+        <button class="btn"> {0} </butoon>
+        <button class="btn"> {0} </butoon>
+    "#, id)
 }
 
 #[tauri::command]
@@ -25,7 +30,7 @@ fn get_message() -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, greet1, get_message])
+        .invoke_handler(tauri::generate_handler![greet, greet1, greet2, get_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
